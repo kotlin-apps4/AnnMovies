43d2e0dbcee08bea0a8949bf7b621778b1a44891
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-05-23 21:38:42 -0500

    Removing Repository methods

--- app/src/main/java/com/example/annmovies/data/repository/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/annmovies/data/repository/movie/MovieRepositoryImpl.kt
@@ -1,22 +1,10 @@
 package com.example.annmovies.data.repository.movie
 
-import com.example.annmovies.domain.model.Movie
 import com.example.annmovies.domain.repository.movie.MovieRepository
-import io.reactivex.rxjava3.core.Single
 
 /**
  * Created by anahi.salgado on 23/03/2020
  */
 class MovieRepositoryImpl: MovieRepository {
-    override fun getPopularMovies(): Single<List<Movie>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun getTopRatedMovies(): Single<List<Movie>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getUpcomingMovies(): Single<List<Movie>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
